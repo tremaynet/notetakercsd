2a1c8dd676e840bb814419a7d5170a88b9b38c88
Tremayne Terry <tremaynet@gmail.com>
2020-11-06 14:47:22 -0500

    defined the sendFile path for /public/notes.html

--- Develop/routes/htmlRoute.js
+++ Develop/routes/htmlRoute.js
@@ -1,8 +1,9 @@
-var router = require("express").Router();
-var path = require("path");
+var router =require("express").Router();
+var path =require("path");
+
 
 router.get("/notes", function(req, res){
-    res,sendFile(path.join(_dirname,"../public/notes.html"))
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
 })
 router.get("*", function(req, res){
     res.sendFile(path.join(__dirname,"../public/index.html"))
